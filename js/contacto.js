c1f836bfb0cc96dfe4484cf097b25caa69753f84
Fernando Hidalgo <fervegaaa_5_@hotmail.com>
2024-06-21 16:23:43 -0300

    Corregido espaciado de botones e indentación

--- js/contacto.js
+++ js/contacto.js
@@ -1,22 +1,24 @@
-(function() {
-    emailjs.init({
-        publicKey: "t2DpVKujmLCqY74vg",
-    });
+(function () {
+	emailjs.init({
+		publicKey: 't2DpVKujmLCqY74vg',
+	});
 })();
 
-window.onload = function() {
-    var form = document.getElementById('contactoForm');
-    var mensajeEnviado = document.getElementById('mensajeEnviado');
+window.onload = function () {
+	var form = document.getElementById('contactoForm');
+	var mensajeEnviado = document.getElementById('mensajeEnviado');
 
-    form.addEventListener('submit', function(event) {
-        event.preventDefault();
-        emailjs.sendForm('service_k23th3q', 'template_bauq4ex', this)
-            .then(() => {
-                console.log('SUCCESS!');
-                mensajeEnviado.style.display = 'block';
-                form.reset();
-            }, (error) => {
-                console.log('FAILED...', error);
-            });
-    });
+	form.addEventListener('submit', function (event) {
+		event.preventDefault();
+		emailjs.sendForm('service_k23th3q', 'template_bauq4ex', this).then(
+			() => {
+				console.log('SUCCESS!');
+				mensajeEnviado.style.display = 'block';
+				form.reset();
+			},
+			(error) => {
+				console.log('FAILED...', error);
+			}
+		);
+	});
 };
